45f99278d21d71b3ce91ac3636516f0a99d2f7e6
Glenboy321 <99928456+Glenboy321@users.noreply.github.com>
2022-02-18 00:22:53 +0000

    Add files via upload

---
+++ javascriptscotland.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    var x = document.getElementById("demo");
+    x.style.fontSize = "25px";           
+    x.style.color = "red"; 
+}
+function MakeItStandOut() {
+    var x = document.getElementById("demo");
+    x.style.fontSize = "25px";           
+    x.style.color = "red"; 
+}
